39f1ae1a691bd3789a662f0aca148f3a734bceb1
imravi20 <rabimajumder08@gmail.com>
2025-01-26 19:03:46 +0530

    updated

--- 5.1.2/src/App.jsx
+++ 5.1.2/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React,{ useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const [count,setCount]=useState(0);
+
+  function updateCount(){
+    setCount(count+1);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button className="card" onClick={updateCount}>Count is {count}</button>
+      <Button count={count} setCount={setCount}></Button>
+      <Button count={count*2} setCount={setCount}></Button>
+      <Button1 count={count} setCount={setCount}></Button1>
+
+
+    </div>
   )
 }
+
+function Button(props){
+  return (
+    <div>
+      <button className='card' onClick={()=>{props.setCount(props.count+1)}}>Count is {props.count}</button>
+    </div>
+  )
+}
+
+class Button1 extends React.Component{
+  constructor(){
+    super();
+  }
+  updateCount() {
+    this.props.setCount(this.props.count+1);
+  }
+  render (){
+    return(
+      <div>
+        <button className='card' onClick={()=>{this.updateCount()}}>Count is {this.props.count}</button>
+      </div>
+    )
+  }
+} 
 
 export default App
